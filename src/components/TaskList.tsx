242e60d1428ad3aa0e1f9a60ffa253faa0f3c0a1
RamonBomfim <ramon.fbomfim@hotmail.com>
2021-07-14 13:07:10 -0300

    Desafio completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.trim() === '') {
+      return;
+    }
+
+    const newTask: Task = {
+      id: Math.floor(Math.random() * 1000),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskIsCompleted = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task);
+    setTasks(taskIsCompleted);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskRemove = tasks.filter(task => task.id !== id);
+    setTasks(taskRemove);
   }
 
   return (
